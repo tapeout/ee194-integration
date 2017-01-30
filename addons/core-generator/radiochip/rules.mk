6c3c1f73eeb2528dd9b6246fe738f2d8c9e35291
Palmer Dabbelt <palmer@dabbelt.com>
2017-01-30 09:09:23 -0800

    Initial build setup
    This adds PLSI, a PLSI addon specific to the radio chip, and some system
    variables that allow the project to run on the ASPIRE machines.

---
+++ addons/core-generator/radiochip/rules.mk
@@ -0,0 +1,3 @@
+# Copyright 2016 Palmer Dabbelt <palmer@dabbelt.com>
+
+include src/addons/core-generator/rocketchip/rules.mk
---
+++ addons/core-generator/radiochip/vars.mk
@@ -0,0 +1,11 @@
+# Copyright 2016 Palmer Dabbelt <palmer@dabbelt.com>
+
+RC_CORE_ADDON_DIRS = $(CORE_GENERATOR_ADDON)/src/cs194-radio-chip $(CORE_GENERATOR_ADDON)/src/sifive-blocks
+
+RC_CORE_CFG_PROJECT = cs194_radio_chip
+RC_CORE_PROJECT = cs194_radio_chip
+
+CORE_TOP = RadioTop
+CORE_SIM_TOP = RadioHarness
+
+include src/addons/core-generator/rocketchip/vars.mk
